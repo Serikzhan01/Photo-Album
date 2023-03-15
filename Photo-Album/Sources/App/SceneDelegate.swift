6cb546614a694aa8ae375d2ed0c75665fa20f0bd
Serikzhan <serikzhan.aubakirov@gmail.com>
2023-03-15 19:32:42 +0600

    project preparation

---
+++ Photo-Album/Sources/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Photo-Album
+//
+//  Created by Serikzhan on 15.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    }
+
+
